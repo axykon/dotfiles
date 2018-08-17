5730c8760b7bc3381ba7e4f5a71e6989be375835
axykon <axykon@gmail.com>
2018-08-17 22:08:39 +0300

    Emacs: add support for go language

--- emacs/init.el
+++ emacs/init.el
@@ -101,6 +101,15 @@
 ;; Go
 (use-package go-mode)
 
+(add-hook 'before-save-hook 'gofmt-before-save)
+(setq-default gofmt-command "goimports")
+;;(add-hook 'go-mode-hook 'go-eldoc-setup)
+(add-hook 'go-mode-hook (lambda ()
+                          (set (make-local-variable 'company-backends) '(company-go))
+                          (company-mode)))
+(add-hook 'go-mode-hook 'yas-minor-mode)
+;;(add-hook 'go-mode-hook 'flycheck-mode)
+
 ;; Direnv
 (use-package direnv
   :config
