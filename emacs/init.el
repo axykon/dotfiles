46e813429cd447ef49525a65f858ef5c692f2615
axykon <axykon@gmail.com>
2018-08-22 16:33:07 +0300

    Emacs: replace winum mode with ace-window

--- emacs/init.el
+++ emacs/init.el
@@ -35,7 +35,8 @@
 (setq package-archive-priorities
       '(("melpa-stable" . 10)
         ("melpa"        . 5)))
-(setq package-pinned-packages '((gruber-darker-theme . "melpa")))
+(setq package-pinned-packages '((gruber-darker-theme . "melpa")
+                                (ace-window . "melpa")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -126,22 +127,9 @@
   :config
   (add-to-list 'eglot-server-programs '(go-mode . ("go-langserver" "-gocodecompletion"))))
 
-;; Winum
-(use-package winum
-  :demand t
-  :bind (("C-`" . winum-select-window-by-number)
-         ("M-0" . winum-select-window-0-or-10)
-         ("M-1" . winum-select-window-1)
-         ("M-2" . winum-select-window-2)
-         ("M-3" . winum-select-window-3)
-         ("M-4" . winum-select-window-4)
-         ("M-5" . winum-select-window-5)
-         ("M-6" . winum-select-window-6)
-         ("M-7" . winum-select-window-7)
-         ("M-8" . winum-select-window-8)
-         ("M-9" . winum-select-window-9))
-  :config
-  (winum-mode))
+;; Ace-window
+(use-package ace-window
+  :bind ("M-o" . ace-window))
 
 ;; Vue
 (use-package vue-mode)
