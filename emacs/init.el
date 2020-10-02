d58ec915bca4ee1f326f9019c26354943b9ea4f3
axykon <axykon@gmail.com>
2020-10-02 23:26:43 +0300

    Set project root based on go.mod

--- emacs/init.el
+++ emacs/init.el
@@ -303,6 +303,28 @@
   :config
   (mood-line-mode 1))
 
+(use-package project
+  :ensure nil
+  :config
+  (defvar project-root-markers '("go.mod")
+    "Files or directories that indicate the root of a project.")
+  (defun aorst/project-find-root (path)
+    "Tail-recursive search in PATH for root markers."
+    (let* ((this-dir (file-name-as-directory (file-truename path)))
+           (parent-dir (expand-file-name (concat this-dir "../")))
+           (system-root-dir (expand-file-name "/")))
+      (cond
+       ((aorst/project-root-p this-dir) (cons 'transient this-dir))
+       ((equal system-root-dir this-dir) nil)
+       (t (aorst/project-find-root parent-dir)))))
+  (defun aorst/project-root-p (path)
+    "Check if current PATH has any of project root markers."
+    (let ((results (mapcar (lambda (marker)
+                             (file-exists-p (concat path marker)))
+                           project-root-markers)))
+      (eval `(or ,@ results))))
+  (add-to-list 'project-find-functions #'aorst/project-find-root))
+
 ;; Additional library path
 (add-to-list 'load-path "~/.emacs.d/lib")
 
