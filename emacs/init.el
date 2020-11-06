6c9e3e65818239e34bb40aed91cded70aa4471f7
axykon <axykon@gmail.com>
2020-11-06 19:33:28 +0300

    Add treemacs package

--- emacs/init.el
+++ emacs/init.el
@@ -303,6 +303,16 @@
   :config
   (mood-line-mode 1))
 
+(use-package treemacs
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
 (use-package project
   :ensure nil
   :config
