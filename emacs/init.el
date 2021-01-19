a65be3b90ccaacfc32c58683711ee28fd4231162
axykon <axykon@gmail.com>
2021-01-19 13:23:21 +0300

    treemacs -> dired-sidebar

--- emacs/init.el
+++ emacs/init.el
@@ -290,10 +290,22 @@
               ("<tab>" . 'dired-subtree-toggle)))
 
 (use-package dired-sidebar
-  :disabled
   :ensure t
   :bind (([f9] . dired-sidebar-toggle-sidebar))
-  :commands (dired-sidebar-toggle-sidebar))
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+  
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'ascii)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
 
 ;; Modeline
 (use-package telephone-line
@@ -310,6 +322,7 @@
   :diminish)
 
 (use-package treemacs
+  :disabled
   :bind
   (:map global-map
         ("M-0"       . treemacs-select-window)
