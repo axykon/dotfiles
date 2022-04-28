f24d8cd5dea2cf73817dcf5f63fff80b1c67a997
axykon <axykon@gmail.com>
2022-04-28 13:25:44 +0300

    Rewrite on_yank autocmd using new lua syntax

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,15 +154,11 @@ vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true,
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
 
 -- Highlight on yank
-vim.api.nvim_exec(
-[[
-augroup YankHighlight
-autocmd!
-autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-augroup end
-]],
-false
-)
+vim.api.nvim_create_augroup('YankHighlight', {})
+vim.api.nvim_create_autocmd('TextYankPost', {
+	group = 'YankHighlight',
+	callback = function() vim.highlight.on_yank() end
+})
 
 --Map blankline
 vim.g.indent_blankline_char = '┊'
