516c89fffa097ebdb5ebb244210f21d6f891b095
axykon <axykon@gmail.com>
2023-01-30 09:59:11 +0300

    Initi with lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,150 +1,30 @@
-require "plugins"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if vim.fn.isdirectory(lazypath) == 0 then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.o.completeopt = 'menu,menuone,noselect'
+vim.g.mapleader = ' '
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.termguicolors = true
-vim.o.completeopt = 'menu'
-vim.o.showcmd = false
+vim.o.completeopt = 'menu,menuone'
 vim.o.showmode = false
-vim.o.signcolumn = 'yes'
-vim.o.scrolloff = 3
-vim.cmd.colorscheme('sonokai')
 
-if vim.g.neovide then
-  -- vim.g.neovide_cursor_trail_legnth = 0
-  -- vim.g.neovide_cursor_animation_length = 1
-  vim.o.guifont = 'Iosevka:h18'
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_fullscreen = true
-end
+require('lazy').setup('plugins')
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function() vim.highlight.on_yank() end
+-- LSP
+vim.api.nvim_create_autocmd('LspAttach', { 
+	group = vim.api.nvim_create_augroup('LSP', {}),
+	desc = 'LSP configuration',
+	callback = function(args) 
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf, silent = true })
+		vim.keymap.set('n', '<leader>o', require('telescope.builtin').lsp_document_symbols, { buffer = args.buf, silent = true })
+	end,
 })
-
-require('mason').setup{}
-require('Comment').setup{}
-
-local luasnip = require('luasnip')
-vim.keymap.set({'i', 's'}, '<Tab>', function() if luasnip.expand_or_jumpable() then luasnip.expand_or_jump() end end, { silent = true })
-vim.keymap.set({'i', 's'}, '<S-Tab>', function() if luasnip.jumpable(-1) then luasnip.jump(-1) end end, {silent = true})
-
-local cmp = require('cmp')
-cmp.setup {
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-		 sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-    })
-}
-
-
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<space>pr', require('telescope').extensions.project.project, opts)
-vim.keymap.set('n', '<space><space>', telescope_builtin.buffers, opts)
-vim.keymap.set('n', '<space>?', telescope_builtin.oldfiles, opts)
-vim.keymap.set('n', '<space>gf', telescope_builtin.git_files, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-
-  vim.keymap.set('n', '<space>ds', telescope_builtin.lsp_document_symbols, bufopts)
-end
-
-local lspconfig = require('lspconfig')
-lspconfig['tsserver'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags
-}
-
-lspconfig['bashls'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags
-}
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-lspconfig['gopls'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-		capabilities = capabilities,
-		settings = {
-			gopls = {
-				usePlaceholders = true
-			}
-		}
-}
-lspconfig['rust_analyzer'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-		capabilities = capabilities,
-    -- Server-specific settings...
-    settings = {
-      ["rust-analyzer"] = {}
-    }
-}
-lspconfig['clojure_lsp'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities,
-}
-
-require("telescope").setup {
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {}
-    },
-		project = {
-			base_dirs = { '~/pro' }
-		}
-  }
-}
-
-require('telescope').load_extension("ui-select")
-require('telescope').load_extension('project')
-
-require('lualine').setup {
-	options = {
-		section_separators = '',
-		component_separators = '',
-	}
-}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,49 +1,115 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
-end
-
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'arcticicestudio/nord-vim'
-	use 'williamboman/mason.nvim'
-	use 'neovim/nvim-lspconfig'
-	use 'mfussenegger/nvim-jdtls'
-	use 'sainnhe/sonokai'
-	use 'aymericbeaumet/vim-symlink'
-	use 'tpope/vim-vinegar'
-	use 'tpope/vim-fugitive'
-	use 'tpope/vim-rhubarb'
-	use 'junegunn/gv.vim'
-	use 'numToStr/Comment.nvim'
-	use 'vim-test/vim-test'
-	use 'ghifarit53/tokyonight-vim'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/nvim-cmp'
-	use 'L3MON4D3/LuaSnip'
-	use 'saadparwaiz1/cmp_luasnip'
-	use 'sainnhe/edge'
-	use 'towolf/vim-helm'
-	use 'tanvirtin/monokai.nvim'
-	use 'airblade/vim-rooter'
-	use 'jbyuki/venn.nvim'
-	use 'nvim-telescope/telescope-ui-select.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = 'nvim-lua/plenary.nvim'
-	}
-	use 'nvim-telescope/telescope-project.nvim'
-	use 'nvim-treesitter/nvim-treesitter'
-	use 'sainnhe/everforest'
-	use 'sainnhe/gruvbox-material'
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+-- local fn = vim.fn
+-- local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+-- if fn.empty(fn.glob(install_path)) > 0 then
+--   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+--   vim.cmd [[packadd packer.nvim]]
+-- end
+-- 
+-- return require('packer').startup(function(use)
+--  use 'wbthomason/packer.nvim'
+--  use 'arcticicestudio/nord-vim'
+--  use 'williamboman/mason.nvim'
+--  use 'neovim/nvim-lspconfig'
+--  use 'mfussenegger/nvim-jdtls'
+--  use 'sainnhe/sonokai'
+--  use 'aymericbeaumet/vim-symlink'
+--  use 'tpope/vim-vinegar'
+--  use 'tpope/vim-fugitive'
+--  use 'tpope/vim-rhubarb'
+--  use 'junegunn/gv.vim'
+--  use 'numToStr/Comment.nvim'
+--  use 'vim-test/vim-test'
+--  use 'ghifarit53/tokyonight-vim'
+--  use 'hrsh7th/cmp-nvim-lsp'
+--  use 'hrsh7th/nvim-cmp'
+--  use 'L3MON4D3/LuaSnip'
+--  use 'saadparwaiz1/cmp_luasnip'
+--  use 'sainnhe/edge'
+--  use 'towolf/vim-helm'
+--  use 'tanvirtin/monokai.nvim'
+--  use 'airblade/vim-rooter'
+--  use 'jbyuki/venn.nvim'
+--  use 'nvim-telescope/telescope-ui-select.nvim'
+--  use {
+--      'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = 'nvim-lua/plenary.nvim'
+--  }
+--  use 'nvim-telescope/telescope-project.nvim'
+--  use 'nvim-treesitter/nvim-treesitter'
+--  use 'sainnhe/everforest'
+--  use 'sainnhe/gruvbox-material'
+--  use {
+--      'nvim-lualine/lualine.nvim',
+--      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+--  }
+--   -- Automatically set up your configuration after cloning packer.nvim
+--   -- Put this at the end after all plugins
+--   if packer_bootstrap then
+--     require('packer').sync()
+--   end
+-- end)
+--
+return {
+  {
+    'folke/tokyonight.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd.colorscheme('tokyonight')
+    end,
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.1',
+    dependencies = { {'nvim-lua/plenary.nvim'}, {'nvim-tree/nvim-web-devicons'} },
+    lazy = false,
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader><space>', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>?', builtin.oldfiles, {})
+	  vim.keymap.set('n', '<leader>g', builtin.git_files)
+    end
+  },
+  {
+    'TimUntersberger/neogit',
+    dependencies = {{ 'nvim-lua/plenary.nvim' }},
+  },
+  {
+    'williamboman/mason.nvim',
+    cmd = 'Mason',
+    config = function()
+		require('mason').setup() 
+		require('mason-lspconfig').setup()
+	end,
+	lazy = false,
+  },
+  {
+	  'williamboman/mason-lspconfig.nvim',
+	  lazy = false,
+  },
+  {
+	  'neovim/nvim-lspconfig',
+	  lazy = false,
+	  config = function()
+		  require('lspconfig').gopls.setup {}
+	  end,
+  },
+  {
+	  'mfussenegger/nvim-jdtls',
+  },
+  {
+	  'weirongxu/plantuml-previewer.vim',
+	  dependencies = {{'tyru/open-browser.vim'}},
+	  config = function()
+		  vim.g['plantuml_previewer#plantuml_jar_path'] = vim.fn.expand('~/.local/lib/java/plantuml.jar')
+		  vim.g['plantuml_previewer#save_format'] = 'svg'
+	  end,
+  },
+  {
+	  'nvim-lualine/lualine.nvim',
+	  dependencies = {{ 'nvim-tree/nvim-web-devicons' }},
+	  config = function()
+		  require('lualine').setup()
+	  end
+  },
+  'tpope/vim-fugitive',
+}
